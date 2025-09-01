5fef52c4cecc7b7d77accc92708bfb6e14daf27b
Felipe José Borges de Mello <159478861+Felipejbm@users.noreply.github.com>
2025-09-01 09:08:44 -0300

    Add files via upload

---
+++ aula0109/estacionamento.sql
@@ -0,0 +1,221 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Tempo de geração: 01-Set-2025 às 14:07
+-- Versão do servidor: 10.4.28-MariaDB
+-- versão do PHP: 8.2.4
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Banco de dados: `estacionamento`
+--
+CREATE DATABASE IF NOT EXISTS `estacionamento` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+USE `estacionamento`;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `andar`
+--
+
+CREATE TABLE `andar` (
+  `codLugar` int(11) NOT NULL,
+  `capacidade` int(11) NOT NULL,
+  `nome_andar` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Extraindo dados da tabela `andar`
+--
+
+INSERT INTO `andar` (`codLugar`, `capacidade`, `nome_andar`) VALUES
+(1, 50, 'Térreo'),
+(2, 40, 'Primeiro'),
+(3, 30, 'Segundo');
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `cliente`
+--
+
+CREATE TABLE `cliente` (
+  `cpf` varchar(20) NOT NULL,
+  `dtnasc` datetime NOT NULL,
+  `nome` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Extraindo dados da tabela `cliente`
+--
+
+INSERT INTO `cliente` (`cpf`, `dtnasc`, `nome`) VALUES
+('32165498700', '1995-01-12 00:00:00', 'Marcos Lima'),
+('45678912345', '1975-06-30 00:00:00', 'Carlos Souza'),
+('78912345678', '1982-11-05 00:00:00', 'Ana Paula'),
+('98765432100', '1990-10-20 00:00:00', 'Maria Oliveira');
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `estaciona`
+--
+
+CREATE TABLE `estaciona` (
+  `cod` int(11) NOT NULL,
+  `horSaida` datetime NOT NULL,
+  `dtEntrada` datetime NOT NULL,
+  `horEntrada` datetime NOT NULL,
+  `dtSaida` datetime NOT NULL,
+  `placa` varchar(7) NOT NULL,
+  `codLugar` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Extraindo dados da tabela `estaciona`
+--
+
+INSERT INTO `estaciona` (`cod`, `horSaida`, `dtEntrada`, `horEntrada`, `dtSaida`, `placa`, `codLugar`) VALUES
+(2, '2025-09-02 15:30:00', '2025-09-02 00:00:00', '0000-00-00 00:00:00', '2025-09-02 00:00:00', 'DEF5678', 2),
+(3, '2025-09-03 18:45:00', '2025-09-03 00:00:00', '0000-00-00 00:00:00', '2025-09-03 00:00:00', 'GHI9012', 3),
+(4, '2025-09-04 09:20:00', '2025-09-04 00:00:00', '0000-00-00 00:00:00', '2025-09-04 00:00:00', 'JKL3456', 1),
+(5, '2025-09-05 20:00:00', '2025-09-05 00:00:00', '0000-00-00 00:00:00', '2025-09-05 00:00:00', 'MNO7890', 2);
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `modelo`
+--
+
+CREATE TABLE `modelo` (
+  `codModelo` int(11) NOT NULL,
+  `modelo` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Extraindo dados da tabela `modelo`
+--
+
+INSERT INTO `modelo` (`codModelo`, `modelo`) VALUES
+(1, 'Sedan'),
+(2, 'SUV'),
+(3, 'Hatch'),
+(4, 'FIAT'),
+(5, 'Moto');
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `veiculo`
+--
+
+CREATE TABLE `veiculo` (
+  `placa` varchar(7) NOT NULL,
+  `cor` varchar(20) NOT NULL,
+  `cpf` varchar(20) NOT NULL,
+  `codModelo` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Extraindo dados da tabela `veiculo`
+--
+
+INSERT INTO `veiculo` (`placa`, `cor`, `cpf`, `codModelo`) VALUES
+('DEF5678', 'Branco', '98765432100', 2),
+('GHI9012', 'Vermelho', '45678912345', 3),
+('JKL3456', 'Azul', '78912345678', 1),
+('MNO7890', 'rosa', '32165498700', 4);
+
+--
+-- Índices para tabelas despejadas
+--
+
+--
+-- Índices para tabela `andar`
+--
+ALTER TABLE `andar`
+  ADD PRIMARY KEY (`codLugar`);
+
+--
+-- Índices para tabela `cliente`
+--
+ALTER TABLE `cliente`
+  ADD PRIMARY KEY (`cpf`);
+
+--
+-- Índices para tabela `estaciona`
+--
+ALTER TABLE `estaciona`
+  ADD PRIMARY KEY (`cod`),
+  ADD KEY `placa` (`placa`),
+  ADD KEY `codLugar` (`codLugar`);
+
+--
+-- Índices para tabela `modelo`
+--
+ALTER TABLE `modelo`
+  ADD PRIMARY KEY (`codModelo`);
+
+--
+-- Índices para tabela `veiculo`
+--
+ALTER TABLE `veiculo`
+  ADD PRIMARY KEY (`placa`),
+  ADD KEY `cpf` (`cpf`),
+  ADD KEY `codModelo` (`codModelo`);
+
+--
+-- AUTO_INCREMENT de tabelas despejadas
+--
+
+--
+-- AUTO_INCREMENT de tabela `andar`
+--
+ALTER TABLE `andar`
+  MODIFY `codLugar` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT de tabela `estaciona`
+--
+ALTER TABLE `estaciona`
+  MODIFY `cod` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT de tabela `modelo`
+--
+ALTER TABLE `modelo`
+  MODIFY `codModelo` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- Restrições para despejos de tabelas
+--
+
+--
+-- Limitadores para a tabela `estaciona`
+--
+ALTER TABLE `estaciona`
+  ADD CONSTRAINT `estaciona_ibfk_1` FOREIGN KEY (`placa`) REFERENCES `veiculo` (`placa`),
+  ADD CONSTRAINT `estaciona_ibfk_2` FOREIGN KEY (`codLugar`) REFERENCES `andar` (`codLugar`);
+
+--
+-- Limitadores para a tabela `veiculo`
+--
+ALTER TABLE `veiculo`
+  ADD CONSTRAINT `veiculo_ibfk_1` FOREIGN KEY (`cpf`) REFERENCES `cliente` (`cpf`),
+  ADD CONSTRAINT `veiculo_ibfk_2` FOREIGN KEY (`codModelo`) REFERENCES `modelo` (`codModelo`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
